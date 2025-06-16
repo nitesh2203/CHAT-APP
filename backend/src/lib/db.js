02b77557d80f733dd3f6ec7ecec996b21d7ff489
nitesh2203 <142528739+nitesh2203@users.noreply.github.com>
2025-06-16 22:00:18 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -12,7 +12,7 @@ export const connectDB = async () => {
 
     //await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/polesDB');
 
-    const conn = await await mongoose.connect(mongodb+srv://nitesh2203:nitesh@22@22@cluster0.bxok7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
+    const conn = await await mongoose.connect("mongodb+srv://nitesh2203:nitesh@22@22@cluster0.bxok7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
