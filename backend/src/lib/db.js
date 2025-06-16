6ee1b3e46bb980f185396bb3e616feecc5a1269f
nitesh2203 <142528739+nitesh2203@users.noreply.github.com>
2025-06-16 21:56:30 +0530

    Update db.js

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -6,12 +6,13 @@ dotenv.config({ path: './src/.env' });
 
 export const connectDB = async () => {
   try {
-    if (!process.env.MONGODB_URI) {
-      throw new Error("❌ MONGODB_URI is not defined in the .env file");
-    }
+    // if (!process.env.MONGODB_URI) {
+    //   throw new Error("❌ MONGODB_URI is not defined in the .env file");
+    // }
 
-    const conn = await mongoose.connect(process.env.MONGODB_URI);
+    //await mongoose.connect(process.env.MONGODB_URI || 'mongodb://127.0.0.1:27017/polesDB');
 
+    const conn = await await mongoose.connect(mongodb+srv://nitesh2203:nitesh@22@22@cluster0.bxok7.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0);
     console.log(`✅ MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.error(`❌ MongoDB connection error: ${error.message}`);
