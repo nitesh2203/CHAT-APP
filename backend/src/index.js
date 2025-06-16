a7fc2a567672fa12eb8e7639aa344009c3309784
nitesh2203 <142528739+nitesh2203@users.noreply.github.com>
2025-06-17 01:10:03 +0530

    Update index.js

--- backend/src/index.js
+++ backend/src/index.js
@@ -18,7 +18,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: "https://chat-app-1-mfzw.onrender.com",
     credentials: true,
   })
 );
